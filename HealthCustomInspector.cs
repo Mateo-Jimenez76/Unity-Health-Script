80d6f09cc288b7ab5ad16e941e5babd08dee6873
MaxGreenhill8529 <tidy-scorer-stem@duck.com>
2025-06-17 08:37:38 -0700

    Added Custom Inspector Buttons

---
+++ HealthCustomInspector.cs
@@ -0,0 +1,31 @@
+using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(Health))]
+public class HealthCustomInspector : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+
+        Health health = (Health)target;
+
+        //Makes inspector settings side by side
+        GUILayout.BeginHorizontal();
+        if (GUILayout.Button("Take Damage"))
+        {
+            health.Damage(1);
+        }
+
+        if (GUILayout.Button("Die"))
+        {
+            health.Damage(health.GetMaxHealth());
+        }
+
+        if(GUILayout.Button("Heal"))
+        {
+            health.Heal(1);
+        }
+        GUILayout.EndHorizontal();
+    }
+}
