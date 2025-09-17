cc8324f2b9ee651cb36e1cae67060f09e75aebf0
MaxGreenhill8529 <tidy-scorer-stem@duck.com>
2025-09-17 11:44:12 -0700

    Update HealthDisplayer.cs
    Added Text Styles and displaying options.

--- HealthDisplayer.cs
+++ HealthDisplayer.cs
@@ -1,18 +1,29 @@
 using UnityEngine;
 using UnityEngine.UI;
-/// <summary>
-/// Script automatically sets up a slider to display the health of the object. Inherits from Health, no need to add Health component separately.
-/// </summary>
 [RequireComponent(typeof(Health))]
 public class HealthDisplayer : MonoBehavior
 {
     [SerializeField] private DisplayType displayType;
-    
+
+    //show if display type is set to slider
     [Header("Slider Settings")]
     [SerializeField] private Slider slider;
     [SerializeField] private Color backgroundColor = Color.red;
     [SerializeField] private Color fillColor = Color.green;
 
+    //Show text settings if display type is set to text
+    [Header("Text Settings")]
+    [SerializeField] private TextMeshProUGUI textObject;
+    [SerializeField] private TextStyle textStyle;
+    //Show if text Style is set to custom
+    [SerializeField] private TextMeshProUGUI maxHealthTextObject;
+    [SerizalizeField] private TextMeshProUGUI currentHealthTextObject;
+
+    private Health health; 
+    private void Awake()
+    {
+        health = GetComponent<Health>();
+    }
 
     private new void OnValidate()
     {
@@ -40,14 +51,34 @@ public class HealthDisplayer : MonoBehavior
 
     private void UpdateHealthDisplay()
     {
-        if(displayType == DisplayType.Slider)
+        if (displayType == DisplayType.Slider)
         {
             slider.value = CurrentHealth;
+            return;
         }
 
-        if(displayType == DisplayType.Text)
+        switch (textStyle)
         {
-            
+            case TextStyle.RawNumber:
+                textObject.text = Health.CurrentHealth;
+                break;
+            case TextStyle.OutOf:
+                textObject.text = health.CurrentHealth.ToString() + "/" + health.GetMaxHealth().ToString();
+                break;
+            case TextStyle.Percentage:
+                textObject.text = (health.CurrentHealth / health.GetMaxHealth()).ToString();
+                break;
+            case TextStyle.Custom:
+                if (maxHealthTextObject != null)
+                {
+                    maxHealthTextObject.text = health.GetMaxHealth();
+                }
+                
+                if (currentHealthTextObject != null)
+                {
+                    currentHealthTextObject.text = health.CurrentHealth();
+                }
+                break;
         }
     }
 
@@ -60,6 +91,17 @@ public class HealthDisplayer : MonoBehavior
     private enum DisplayType
     {
         Slider,
-        Text
+        Text,
+    }
+
+    private enum TextStyle
+    {
+        //5
+        RawNumber,
+        // 5/10
+        OutOf,
+        //50%
+        Percentage,
+        Custom,
     }
 }
